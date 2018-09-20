6c00d82cdf03db488ddc6f2586975ff7010a5d60
Bryan Brophy <brybrophy@gmail.com>
2018-09-19 23:11:59 -0700

    Update d.ts file

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,13 +1,26 @@
-declare module "react-progressive-image"  {
-    export interface ProgressiveImageProps {
-        onError?: (errorEvent: Event) => void;
-        placeholder: string;
-        src: string;
-    }
+declare module 'react-progressive-image' {
+  export interface ProgressiveImageProps {
+    delay?: number;
+    onError?: (errorEvent: Event) => void;
+    placeholder: string;
+    src: string;
+    srcSetData?: {
+      srcSet: string;
+      sizes: string;
+    };
+  }
 
-    export interface ProgressiveImageState {
-        image: string;
-    }
+  export interface ProgressiveImageState {
+    image: string;
+    loading: boolean;
+    srcSetData?: {
+      srcSet: string;
+      sizes: string;
+    };
+  }
 
-    export default class ProgressiveImage extends React.Component<ProgressiveImageProps, ProgressiveImageState>{}
+  export default class ProgressiveImage extends React.Component<
+    ProgressiveImageProps,
+    ProgressiveImageState
+  > {}
 }
